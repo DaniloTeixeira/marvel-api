312c21751582fdfd58178d5691c989b7aac87ee7
Danilo Teixeira <danilo.rt@outlook.com>
2024-04-05 00:34:02 -0300

    feat: create loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { LoaderService } from '../services/loader';
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent {
+  showLoader$ = inject(LoaderService).showLoader$;
+}
