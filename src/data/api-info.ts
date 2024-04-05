93998ceb313a7316c9d0d859a3169ee79a35a440
Danilo Teixeira <danilo.rt@outlook.com>
2024-04-05 00:19:29 -0300

    feat: add data to api request

---
+++ src/data/api-info.ts
@@ -0,0 +1,6 @@
+export const apiInfo = {
+  publicKey: '060bf3fb8e4a3bf4040e8567f49bbdc2',
+  privateKey: 'c905d32fc80e1cf1b64d385d8a732fdd3359031d',
+  apiBaseURL: 'https://gateway.marvel.com/v1/public',
+  timestamp: Date.now().toString(),
+};
