3245e9c056ee99f690ba310a083b0bf1adbb7d38
Danilo Teixeira <danilo.rt@outlook.com>
2024-04-06 03:58:58 -0300

    feat: create footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/components/footer';
 import { LoaderComponent } from './core/components/loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent],
+  imports: [RouterOutlet, LoaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
